e68ac590bbc456949a389a60e93e571ef6bbaee0
Ilija Ilievski <ilija139@gmail.com>
2014-08-24 21:01:27 +0800

    Initial commit

---
+++ src/sg/edu/nus/StackOverflow/servlet/Test.java
@@ -0,0 +1,27 @@
+package sg.edu.nus.StackOverflow.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet(
+        description = "Test servlet to test that the web server is up and that the MongoDB connection is working.",
+        urlPatterns = { "/test" })
+public class Test extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().print("Server is OK!");
+    }
+
+}
