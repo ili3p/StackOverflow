1948c2bbdc3ed34d41f7315dfd09faa0f295e36f
Ilija Ilievski <ilija139@gmail.com>
2014-08-24 21:21:01 +0800

    Gson library added

--- src/sg/edu/nus/StackOverflow/util/DB.java
+++ src/sg/edu/nus/StackOverflow/util/DB.java
@@ -10,9 +10,13 @@ public enum DB {
     private static final MongoClient mc = getClient();
     private static final String DB_NAME = "stackoverflow";
 
+    /**
+     * Testing that the mongoDB connection is working.
+     * Note that you need manually to insert record in the test collection in the MongoDB database during installation. 
+     * @return
+     */
     public static String test() {
         DBCollection coll = mc.getDB(DB_NAME).getCollection("test");
-
         return coll.find().next().toString();
     }
 
